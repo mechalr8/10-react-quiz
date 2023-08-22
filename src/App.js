e61a2a4e966cd4efcb885f5104fb9dcfd6f864ba
mechalr8 <harsitagrawal59@gmail.com>
2023-08-22 18:46:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,167 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useReducer } from "react";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Loader from "./components/Loader";
+import Error from "./components/Error";
+import StartScreen from "./components/StartScreen";
+import Question from "./components/Question";
+import NextButton from "./components/NextButton";
+import Progress from "./components/Progress";
+import FinishScreen from "./components/FinishScreen";
+import Footer from "./components/Footer";
+import Timer from "./components/Timer";
+
+const initialState = {
+    questions: [],
+    // 'loading', 'error', 'ready', 'active', 'finished'
+    status: "loading",
+    index: 0,
+    answer: null,
+    points: 0,
+    highscore: 0,
+    secondsRemaining: null,
+};
+
+const SECS = 30
+
+function reducer(state, action) {
+    switch (action.type) {
+        case "dataReceived":
+            return {
+                ...state,
+                questions: action.payload,
+                status: "ready",
+            };
+        case "dataFailed":
+            return {
+                ...state,
+                status: "error",
+            };
+        case "start":
+            return {
+                ...state,
+                status: "active",
+                secondsRemaining: state.questions.length * SECS
+            };
+        case "newAnswer":
+            const question = state.questions[state.index];
+            return {
+                ...state,
+                answer: action.payload,
+                points:
+                    action.payload === question.correctOption
+                        ? state.points + question.points
+                        : state.points,
+            };
+        case "nextQuestion":
+            return {
+                ...state,
+                index: state.index + 1,
+                answer: null,
+            };
+        case "finish":
+            return {
+                ...state,
+                status: "finished",
+                highscore:
+                    state.points > state.highscore
+                        ? state.points
+                        : state.highscore,
+            };
+        case "restart":
+            return {
+                ...initialState,
+                questions: state.questions,
+                status: "ready",
+            };
+        case "tick":
+            return {
+                ...state,
+                secondsRemaining: state.secondsRemaining - 1,
+                status:
+                    state.secondsRemaining === 0 ? "finished" : state.status,
+            };
+        default:
+            throw new Error("Unknown Action");
+    }
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [
+        {
+            questions,
+            status,
+            index,
+            answer,
+            points,
+            highscore,
+            secondsRemaining,
+        },
+        dispatch,
+    ] = useReducer(reducer, initialState);
+    const numQuestions = questions.length;
+    const totalPoints = questions.reduce((crr, acc) => crr + acc.points, 0);
+
+    useEffect(() => {
+        fetch("http://localhost:8000/questions")
+            .then((res) => res.json())
+            .then((data) => dispatch({ type: "dataReceived", payload: data }))
+            .catch((err) =>
+                dispatch({ type: "dataFailed", payload: err.message })
+            );
+    }, []);
+
+    return (
+        <div className='app'>
+            <Header />
+            <Main>
+                {status === "loading" && <Loader />}
+                {status === "error" && <Error />}
+                {status === "ready" && (
+                    <StartScreen
+                        numQuestions={numQuestions}
+                        dispatch={dispatch}
+                    />
+                )}
+                {status === "active" && (
+                    <>
+                        <Progress
+                            index={index}
+                            numQuestions={numQuestions}
+                            points={points}
+                            totalPoints={totalPoints}
+                            answer={answer}
+                        />
+                        <Question
+                            question={questions[index]}
+                            dispatch={dispatch}
+                            answer={answer}
+                        />
+                        <Footer>
+                            <Timer
+                                dispatch={dispatch}
+                                secondsRemaining={secondsRemaining}
+                            />
+                            <NextButton
+                                dispatch={dispatch}
+                                answer={answer}
+                                index={index}
+                                numQuestions={numQuestions}
+                            />
+                        </Footer>
+                    </>
+                )}
+                {status === "finished" && (
+                    <FinishScreen
+                        points={points}
+                        totalPoints={totalPoints}
+                        highscore={highscore}
+                        dispatch={dispatch}
+                    />
+                )}
+            </Main>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/FinishScreen.js
@@ -0,0 +1,26 @@
+function FinishScreen({ points, totalPoints, highscore, dispatch }) {
+    const percentage = (points * 100) / totalPoints;
+    let emoji;
+    if (percentage === 100) emoji = "🥇";
+    if (percentage >= 80 && percentage < 100) emoji = "🎉";
+    if (percentage >= 50 && percentage < 80) emoji = "🙃";
+    if (percentage > 0 && percentage < 50) emoji = "🤨";
+    if (percentage === 0) emoji = "🤦‍♂️";
+    return (
+        <>
+            <p className='result'>
+                <span>{emoji}</span> You scored <strong>{points}</strong> points
+                out of {totalPoints}. ({Math.ceil(percentage)}%)
+            </p>
+            <p className='highscore'>(Highscore: {highscore} points)</p>
+            <button
+                className='btn btn-ui'
+                onClick={() => dispatch({ type: "restart" })}
+            >
+                Restart Quiz
+            </button>
+        </>
+    );
+}
+
+export default FinishScreen;
